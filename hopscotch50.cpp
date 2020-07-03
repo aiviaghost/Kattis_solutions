d760efba20a3532a48b4ab6eada9c1aacfe7cf0c
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2020-07-03 21:29:20 +0200

    yes

--- hopscotch50.cpp
+++ hopscotch50.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 using pii = pair<int, int>;
 using pip = pair<int, pii>;
-using pipip = pair<int, pip>;
 
 auto get_manhattan_dist(pii p1, pii p2) -> int {
     return abs(p1.first - p2.first) + abs(p1.second - p2.second);
@@ -16,43 +15,54 @@ auto main() -> int {
     int n, k;
     cin >> n >> k;
 
-    unordered_map<int, vector<pip>> adj;
+    vector<vector<int>> grid(n, vector<int>(n));
+    unordered_map<int, vector<pii>> adj;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             int val;
             cin >> val;
-            adj[val].push_back({val, {j, i}});
+            adj[val].push_back({j, i});
+            grid[i][j] = val;
         }
     }
 
     int shortest_dist = numeric_limits<int>::max();
 
-    for (pip s_node : adj[1]) { // run dijkstra from every 1-node
-        vector<vector<int>> dist(n, vector<int>(n, numeric_limits<int>::max())), vis(n, vector<int>(n, -1));
-        dist[s_node.second.second][s_node.second.first] = 0;
-        priority_queue<pipip, vector<pipip>, greater<pipip>> pq;
-        pq.push({0, {s_node.first, {s_node.second.first, s_node.second.second}}});
-
-        while (!pq.empty()) {
-            pipip curr = pq.top();
-            pq.pop();
-
-            if (vis[curr.second.second.second][curr.second.second.first] != -1) {
-                continue;
+    if (!adj[1].empty()) {
+        for (pii s_node : adj[1]) { // run dijkstra from every 1-node
+            if (grid[s_node.second][s_node.first] == k) {
+                shortest_dist = 0;
+                break;
             }
 
-            vis[curr.second.second.second][curr.second.second.first] = 1;
-            
-            for (pip next : adj[curr.second.first + 1]) {
-                int alt_dist = dist[curr.second.second.second][curr.second.second.first] + get_manhattan_dist(curr.second.second, next.second);
-                if (alt_dist < dist[next.second.second][next.second.first]) {
-                    shortest_dist = (curr.first + 1 == k ? min(alt_dist, shortest_dist) : shortest_dist);
-                    dist[next.second.second][next.second.first] = alt_dist;
-                    pq.push({dist[next.second.second][next.second.first], {next.first, {next.second.first, next.second.second}}});
+            vector<vector<int>> dist(n, vector<int>(n, numeric_limits<int>::max())), vis(n, vector<int>(n, -1));
+            dist[s_node.second][s_node.first] = 0;
+            priority_queue<pip, vector<pip>, greater<pip>> pq;
+            pq.push({0, {s_node.first, s_node.second}});
+
+            while (!pq.empty()) {
+                pip curr = pq.top();
+                pq.pop();
+
+                if (vis[curr.second.second][curr.second.first] != -1) {
+                    continue;
+                }
+
+                vis[curr.second.second][curr.second.first] = 1;
+
+                if (!adj[grid[curr.second.second][curr.second.first] + 1].empty()) {
+                    for (pii next : adj[grid[curr.second.second][curr.second.first] + 1]) {
+                        int alt_dist = dist[curr.second.second][curr.second.first] + get_manhattan_dist(curr.second, next);
+                        if (alt_dist < dist[next.second][next.first]) {
+                            shortest_dist = (grid[next.second][next.first] == k ? min(alt_dist, shortest_dist) : shortest_dist);
+                            dist[next.second][next.first] = alt_dist;
+                            pq.push({dist[next.second][next.first], {next.first, next.second}});
+                        }
+                    }
                 }
             }
         }
     }
-
+    
     cout << (shortest_dist == numeric_limits<int>::max() ? "-1" : to_string(shortest_dist)) << "\n";
 }
