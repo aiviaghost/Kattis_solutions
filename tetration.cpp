a5697472ae40a8b35ca9c0b7829e7ce00456fac0
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2020-07-12 22:34:44 +0200

    yes

---
+++ tetration.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+auto main() -> int {
+    cin.tie(0)->sync_with_stdio(0);
+    cout << fixed << setprecision(6);
+
+    double n;
+    cin >> n;
+
+    cout << pow(n, 1 / n);
+}
