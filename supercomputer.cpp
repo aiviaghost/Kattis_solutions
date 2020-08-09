260d38adea212bfa1b20cb6d5d28f46dd93b03dd
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2020-08-09 02:29:59 +0200

    first segment tree

--- supercomputer.cpp
+++ supercomputer.cpp
@@ -4,51 +4,30 @@ using namespace std;
 
 struct segment_tree {
     segment_tree *left, *right;
-    int state, value, l_bound, r_bound;
-    bool is_leaf = false;
-    segment_tree(int l_bound, int r_bound): l_bound(l_bound), r_bound(r_bound), left(NULL), right(NULL), state(0), value(0) { }
+    int state = 0, value = 0, lb, rb;
+    segment_tree(int lb, int rb) : lb(lb), rb(rb), left(NULL), right(NULL) {
+        if (lb < rb) {
+            int mid = lb + (rb - lb) / 2;
+            left = new segment_tree(lb, mid);
+            right = new segment_tree(mid + 1, rb);
+        }
+    }
 };
-
-auto dfs(segment_tree *tree) -> void {
-    cout << "l: " << tree->l_bound << ", r: " << tree->r_bound << ", state: " << tree->state << ", val: " << tree->value << "\n";
-    dfs(tree->left);
-    dfs(tree->right);
-}
-
-auto build_seg_tree(int l, int r) -> segment_tree* {
-    if (l > r) {
-        return NULL;
-    }
-
-    segment_tree *tree = new segment_tree(l, r);
-    cout << tree->l_bound << " " << tree->r_bound << endl;
-
-    if (l == r) {
-        tree->is_leaf = true;
-    }
-    else {
-        int mid = (l + r) / 2;
-        tree->left = build_seg_tree(l, mid);
-        tree->right = build_seg_tree(mid + 1, r);
-    }
-
-    return tree;
-}
 
 auto query(segment_tree *tree, int l, int r) -> int {
     if (tree == NULL) {
         return 0;
     }
 
-    if (tree->l_bound <= l && r <= tree->r_bound) {
+    if (l <= tree->lb && tree->rb <= r) {
         return tree->value;
     }
 
-    if (tree->l_bound > r) {
+    if (tree->rb < l) {
         return 0;
     }
 
-    if (tree->r_bound < l) {
+    if (tree->lb > r) {
         return 0;
     }
 
@@ -59,18 +38,14 @@ auto update(segment_tree *tree, int index) -> int {
     if (tree == NULL) {
         return 0;
     }
-
-    if (tree->l_bound > index) {
-        return 0;
+    
+    if (tree->lb > index || tree->rb < index) {
+        return tree->value;
     }
 
-    if (tree->r_bound < index) {
-        return 0;
-    }
-
-    if (tree->l_bound == tree->r_bound && tree->l_bound == index) {
+    if (tree->lb == tree->rb && tree->lb == index) {
         tree->state = 1 - tree->state;
-        
+
         if (tree->state == 1) {
             tree->value++;
         }
@@ -86,13 +61,12 @@ auto update(segment_tree *tree, int index) -> int {
 }
 
 auto main() -> int {
-    // cin.tie(0)->sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(0);
 
     int n, k;
     cin >> n >> k;
 
-    segment_tree *seg_tree = build_seg_tree(1, n);
-    dfs(seg_tree);
+    segment_tree *seg_tree = new segment_tree(1, n);
     
     while (k--) {
         char op;
