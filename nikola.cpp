e014e46247ec813e6061324457f27b981a6ce948
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2020-09-23 00:50:16 +0200

    I am not so dumb after all

--- nikola.cpp
+++ nikola.cpp
@@ -21,18 +21,17 @@ auto main() -> int {
     int sum[n][n];
     memset(sum, -1, 4 * n * n);
     sum[1][1] = cost[1];
-    priority_queue<pair<int, pii>, vector<pair<int, pii>>, greater<pair<int, pii>>> q;
-    q.push({cost[1], {1, 1}});
+    queue<pii> q;
+    q.push({1, 1});
     while (!q.empty()) {
-        pair<int, pii> curr = q.top();
+        pii curr = q.front();
         q.pop();
-
-        int adj[2] = {-curr.second.second, curr.second.second + 1};
+        int adj[2] = {-curr.second, curr.second + 1};
         for (int i = 0; i < 2; i++) {
-            int next = curr.second.first + adj[i];
-            if (0 <= next && next < n && sum[next][abs(adj[i])] == -1) {
-                sum[next][abs(adj[i])] = sum[curr.second.first][curr.second.second] + cost[next];
-                q.push({sum[next][abs(adj[i])], {next, abs(adj[i])}});
+            int next = curr.first + adj[i];
+            if (0 <= next && next < n && (sum[next][abs(adj[i])] == -1 || sum[next][abs(adj[i])] > sum[curr.first][curr.second] + cost[next])) {
+                sum[next][abs(adj[i])] = sum[curr.first][curr.second] + cost[next];
+                q.push({next, abs(adj[i])});
             }
         }
     }
