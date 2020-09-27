bf54c1570be0468a4797343f6406adde9ecc4122
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2020-09-27 22:24:47 +0200

    sync no good when scanf() / printf()

--- subway2.cpp
+++ subway2.cpp
@@ -17,7 +17,7 @@ struct edge {
 };
 
 auto main() -> int {
-    cin.tie(0)->sync_with_stdio(0);
+    // cin.tie(0)->sync_with_stdio(0);
     
     int x1, y1, x2, y2;
     cin >> x1 >> y1 >> x2 >> y2;
