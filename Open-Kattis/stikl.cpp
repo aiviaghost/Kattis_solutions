f4d0d3cf4af0bd03f5253f9ecb4bc6a13ccdf32f
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2021-08-21 05:09:18 +0200

    Probably hardest sole yet! Segment tree + binary lifting.
    Use max-segment-tree to find, for each i the leftmost element in the interval [i + 1, n] that is greater than or equal to a[i]. Use this to link up all the tiles (and an artificial tile) and use binary lifting to query.

--- Open-Kattis/stikl.cpp
+++ Open-Kattis/stikl.cpp
@@ -16,58 +16,44 @@ const int INF = 2e9;
 const int MOD = 1e9 + 7;
 
 struct segment_tree {
-    int lb, rb, val, max_lb, max_rb;
+    int lb, rb, val;
     segment_tree *left, *right;
     
     segment_tree(vector<int>& a, int lb, int rb) {
         this->lb = lb;
         this->rb = rb;
-        if (lb - rb == 0) {
+        if (lb == rb) {
             this->left = this->right = NULL;
             this->val = a[lb];
-            this->max_lb = this->max_rb = lb;
         }
         else {
             int mid = lb + (rb - lb) / 2;
             this->left = new segment_tree(a, lb, mid);
             this->right = new segment_tree(a, mid + 1, rb);
             this->val = max(this->left->val, this->right->val);
-            if (this->left->val > this->right->val) {
-                this->max_lb = this->left->max_lb;
-                this->max_rb = this->left->max_rb;
-            }
-            else if (this->right->val > this->left->val) {
-                this->max_lb = this->right->max_lb;
-                this->max_rb = this->right->max_rb;
-            }
-            else {
-                this->max_lb = this->left->max_lb;
-                this->max_rb = this->right->max_rb;
-            }
         }
     }
-
+	
     auto query(int lb, int rb, int min_val) -> int {
-        /*cout << "curr: " << this->lb << " " << this->rb << endl;
-        cout << lb << " " << rb << endl;
-        if (this->left != NULL && this->right != NULL) {
-            cout << "left: " << this->left->lb << " " << this->left->rb << ", right: " << this->right->lb << " " << this->right->rb << endl;
-            cout << "max: " << this->left->val << " " << this->right->val << endl;
-            cout << "index of max: " << this->left->index_of_max << " " << this->right->index_of_max << endl;
-        }*/
-
         if (this->lb == this->rb) {
-            return this->lb;
+            return this->val >= min_val && lb <= this->lb && this->rb <= rb ? this->lb : INF;
         }
 
-        if (this->left->val >= min_val && lb <= this->left->max_rb) {
-            // cout << "1\n";
-            return this->left->query(lb, rb, min_val);
-        }
-        else {
-            // cout << "2\n";
-            return this->right->query(lb, rb, min_val);
-        }
+		if (this->rb < lb) {
+			return INF;
+		}
+
+        int ret = INF;
+		
+		if (this->left->val >= min_val) {
+			ret = this->left->query(lb, rb, min_val);
+		}
+
+		if (ret == INF && this->right->val >= min_val) {
+			ret = this->right->query(lb, rb, min_val);
+		}
+
+		return ret;
     }
 };
 
@@ -81,32 +67,24 @@ auto main() -> int {
         cin >> a[i];
     }
     a[n] = INF; // artificial node to create connected tree
-    segment_tree *tree = new segment_tree(a, 0, n);
-    /*cout << "finished tree!\n";
-    for (int i = 0; i < n; i++) {
-        // cout << "target: " << i + 1 << " " << n << endl;
-        cout << "{" << tree->query(i + 1, n, a[i]) << "} ";
-    }
-    cout << endl;*/
-    int max_log = 0;
+    
+	segment_tree *tree = new segment_tree(a, 0, n);
+    
+	int max_log = 0;
     while (1 << max_log <= n + 1) {
         max_log++;
     }
+
     auto dp = make<int>(0, n + 1, max_log + 1);
-    for (int i = 0; i <= n ; i++) {
+    for (int i = 0; i < n ; i++) {
         dp[i][0] = tree->query(i + 1, n, a[i]);
     }
+	dp[n][0] = n;
     for (int i = 1; i <= max_log; i++) {
         for (int v = 0; v <= n; v++) {
             dp[v][i] = dp[dp[v][i - 1]][i - 1];
         }
     }
-    /*for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= max_log; j++) {
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
-    }*/
 
     while (q--) {
         int s, d;
