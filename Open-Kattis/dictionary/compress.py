29fa81c421756a69c57acfc932486a7b630296a5
aiviaghost <smurfplatta@gmail.com>
2022-05-20 23:11:30 +0200

    Add "bytes+offset" version of compress.py

--- Open-Kattis/dictionary/compress.py
+++ Open-Kattis/dictionary/compress.py
@@ -19,38 +19,44 @@ class Trie:
         
 
 with open("dict.txt") as f:
-    words = f.read().strip().split("\n")
+    words = f.read().strip().encode().split(b"\n")
 
 T = Trie()
-for word in words[ : 98660]:
-    T.insert(word.replace("'s", "&"))
+for word in words:
+    T.insert(word.replace(b"'s", b"&"))
 
 def dfs(t):
     if len(t.children) == 0:
         return {}
     curr = {}
     for k, v in t.children.items():
-        curr[k + ("a" if v.is_end else "")] = dfs(v)
+        curr[bytes([k + (128 if v.is_end else 0)])] = dfs(v)
     return curr
 
-s = str(dfs(T)).replace(" ", "")
+s = str(dfs(T)).encode().replace(b" ", b"")
 
+"""
 with open("original.txt", "w") as f:
     f.write(s)
-
-s = s.encode()
+"""
+# s = s.encode()
 
 with open("bin.txt", "wb") as f:
     sub1 = re.sub(
-            pattern = b"'[a-zA-Z&'](a|)'", 
-            repl = lambda x: x.group()[1 : -1], 
+            pattern = b"b\\'[a-zA-Z&']\\'", 
+            repl = lambda x: x.group()[2 : -1], 
             string = s
         )
     sub2 = re.sub(
-            pattern = b'"[a-zA-Z\'](a|)"', 
-            repl = lambda x: x.group()[1 : -1], 
+            pattern = b'b"[a-zA-Z\']"', 
+            repl = lambda x: x.group()[2 : -1], 
             string = sub1
         ).replace(b"{}", b"").replace(b":", b"").replace(b"''", b"")
+    sub3 = re.sub(
+        pattern = b"b'[\\a-zA-Z0-9]{4,4}'",
+        repl = lambda x: x.group()[4 : -1],
+        string = sub2
+    )
     f.write(
-        gzip.compress(bz2.compress(sub2))
+        gzip.compress(bz2.compress(sub3))
     )
