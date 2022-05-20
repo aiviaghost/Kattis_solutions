226ed84587cb770af1e195e1db68cac7d5f13224
aiviaghost <smurfplatta@gmail.com>
2022-05-21 01:56:57 +0200

    Decrease size of dictionary.py

--- Open-Kattis/dictionary/dictionary.py
+++ Open-Kattis/dictionary/dictionary.py
@@ -1,8 +1,8 @@
 import bz2,re,json,gzip
 w=[]
 def t(d,c,a=0):
-    if a:w.append(c)
-    for k,v in d.items():t(v,c+k[0],len(k)>1)
+	if a:w.append(c)
+	for k in d:t(d[k],c+k[0],len(k)>1)
 p="[a-zA-Z&']"
 f=re.sub
 t(json.loads(f(":(,|})",lambda x:':{}'+x.group()[1:],f(p+"{",lambda x:'"'+x.group()[:-1]+'":{',f(p+"a",lambda x:f'"{x.group()}":',bz2.decompress(gzip.decompress(open("/src/aa","rb").read())).decode())))),"")
