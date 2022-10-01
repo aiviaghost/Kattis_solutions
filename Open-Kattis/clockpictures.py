6982e4e0909627a155421914ffe89af685d84cdb
aiviaghost <smurfplatta@gmail.com>
2022-10-01 03:02:32 +0200

    Refactor clockpictures, add note on implementation in other languages

--- Open-Kattis/clockpictures.py
+++ Open-Kattis/clockpictures.py
@@ -9,13 +9,15 @@ def hash(s):
 n = int(input())
 a, b = sorted(map(int, input().split())), sorted(map(int, input().split()))
 
-ad, bd = [(a[(i + 1) % n] - a[i] + B) % B for i in range(n)], [(b[(i + 1) % n] - b[i] + B) % B for i in range(n)]
+# Modulo operators in other languages might return negative results which might be unwanted. If so add the base (B) before mod.
+# ad, bd = [(a[(i + 1) % n] - a[i] + B) % B for i in range(n)], [(b[(i + 1) % n] - b[i] + B) % B for i in range(n)]
+ad, bd = [(a[(i + 1) % n] - a[i]) % B for i in range(n)], [(b[(i + 1) % n] - b[i]) % B for i in range(n)]
 
 ha, hb = hash(ad), hash(bd)
 ok = ha == hb
 if not ok:
     for i in range(n):
-        hb = (hb * B - bd[i] * pow(B, n, P) + bd[i]) % P
+        hb = (hb * B - bd[i] * pow(B, n, P) + bd[i]) % P # might require similar trick as above in other languages.
         if ha == hb:
             ok = True
             break
