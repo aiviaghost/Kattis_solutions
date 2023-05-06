e8e7a05913108b57886292ee5958a16cf75aa8d2
aiviaghost <smurfplatta@gmail.com>
2023-05-07 01:25:49 +0200

    Refactor sheldon

--- Open-Kattis/sheldon.py
+++ Open-Kattis/sheldon.py
@@ -3,7 +3,7 @@ X, Y = map(int, input().split())
 nums = set()
 
 for n in range(1, 64):
-    for m in range(1, 64):
+    for m in range(1, 63):
         for k in range(1, 64):
             num = "".join("1" * n if i % 2 == 0 else "0" * m for i in range(k))
             if len(num) >= 64:
