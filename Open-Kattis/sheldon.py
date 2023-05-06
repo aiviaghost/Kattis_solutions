1fd5b54a859c2cf835e356e043e0053af14f59f1
aiviaghost <smurfplatta@gmail.com>
2023-05-07 01:10:44 +0200

    Solve sheldon

---
+++ Open-Kattis/sheldon.py
@@ -0,0 +1,15 @@
+X, Y = map(int, input().split())
+
+nums = set()
+
+for n in range(1, 64):
+    for m in range(1, 64):
+        for k in range(1, 64):
+            num = "".join("1" * n if i % 2 == 0 else "0" * m for i in range(k))
+            if len(num) >= 64:
+                break
+            num = int(num, 2)
+            if X <= num <= Y:
+                nums.add(num)
+
+print(len(nums))
