7dfd1931f1eb23bd8d94c8f270c6ba59fae0b26c
aiviaghost <smurfplatta@gmail.com>
2024-11-16 01:14:44 +0100

    Solve kinversions

---
+++ Open-Kattis/kinversions.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+// #define unordered_map gp_hash_table
+
+#define int ll
+#define all(xs) xs.begin(), xs.end()
+// #define in(xs, a) (xs.find(a) != xs.end())
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using vi = vector<int>;
+
+template <typename T> vector<T> make(T init, size_t size) { return vector<T>(size, init); }
+template <typename T, typename... Args> auto make(T init, size_t first, Args... sizes) {
+    auto inner = make<T>(init, sizes...);
+    return vector<decltype(inner)>(first, inner);
+}
+
+const int INF = 2e9;
+const int MOD = 1e9 + 7;
+
+typedef complex<double> C;
+typedef vector<double> vd;
+void fft(vector<C>& a) {
+    int n = sz(a), L = 31 - __builtin_clz(n);
+    static vector<complex<long double>> R(2, 1);
+    static vector<C> rt(2, 1);  // (^ 10% faster if double)
+    for (static int k = 2; k < n; k *= 2) {
+        R.resize(n); rt.resize(n);
+        auto x = polar(1.0L, acos(-1.0L) / k);
+        rep(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
+    }
+    vi rev(n);
+    rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+    rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
+            // C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
+            auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];        /// exclude-line
+            C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);           /// exclude-line
+            a[i + j + k] = a[i + j] - z;
+            a[i + j] += z;
+        }
+}
+vd conv(const vd& a, const vd& b) {
+    if (a.empty() || b.empty()) return {};
+    vd res(sz(a) + sz(b) - 1);
+    int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+    vector<C> in(n), out(n);
+    copy(all(a), begin(in));
+    rep(i,0,sz(b)) in[i].imag(b[i]);
+    fft(in);
+    for (C& x : in) x *= x;
+    rep(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
+    fft(out);
+    rep(i,0,sz(res)) res[i] = imag(out[i]) / (4 * n);
+    return res;
+}
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string s;
+    cin >> s;
+
+    reverse(all(s));
+    int n = sz(s);
+
+    int max_positive_exp = 0;
+    int max_negative_exp = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 'A') {
+            max_negative_exp = i;
+        }
+        else {
+            max_positive_exp = i;
+        }
+    }
+
+    vector<double> p1(max_negative_exp + 1 + max_positive_exp), p2(max_negative_exp + 1);
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 'B') {
+            p1[max_negative_exp + i]++;
+        }
+        else {
+            p2[max_negative_exp + (-i)]++;
+        }
+    }
+
+    auto ans = conv(p1, p2);
+    for (int i = 2 * max_negative_exp + 1; i < 2 * max_negative_exp + n; i++) {
+        cout << (i < sz(ans) ? lround(ans[i]) : 0) << "\n";
+    }
+}
