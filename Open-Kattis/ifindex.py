741a9f95fe8dd1a86a6242012f62d9b233a0f6f1
aiviaghost <smurfplatta@gmail.com>
2025-12-19 22:36:46 +0100

    Solve ifindex

---
+++ Open-Kattis/ifindex.py
@@ -0,0 +1,15 @@
+from math import floor
+
+depths = []
+curr_depth = 0
+for c in input():
+    if c == "{":
+        curr_depth += 1
+    elif c == "}":
+        curr_depth -= 1
+    elif c == "i":
+        depths.append(curr_depth)
+
+ans = sorted(depths)[floor((len(depths) + 1) // 2) - 1] if depths else -1
+
+print(ans)
